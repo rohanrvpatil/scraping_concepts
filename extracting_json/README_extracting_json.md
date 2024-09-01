c3aef87d6160c9b9dd38b6c1466af3732a2bb3e9
Rohan Patil <rohan.rv.patil@gmail.com>
2024-09-01 23:56:01 +0530

    feat: functioning project

--- extracting_json/README_extracting_json.md
+++ extracting_json/README_extracting_json.md
@@ -8,3 +8,9 @@ Implementation steps:
 5) Visited curl convertor website and pasted the curl command and generated the python code
 6) Copied the code in the project and generated the response json
 7) Converted the json into a xlsx file
+
+Tips:
+1) Experiment with limit, start with limit=100 and go higher
+2) yielding is a good way to avoid lists of lists
+3) To get params, visit Fetch/XHR in Network Tab. The URL with params will appear after the Search button has
+been clicked
--- extracting_json/main.py
+++ extracting_json/main.py
@@ -1,5 +1,20 @@
 from src import generate_json, export_json
+import json
 
-#generate_json()
 
+results=[]
+
+#storing all products in results
+#5000 is used since there are 4870 search results for the term 'dog'
+# 100 can be tested with different values
+for offset in range(0,5000,40):
+    for product in generate_json(offset):
+        results.append(product)
+        
+# generating json file        
+with open("./files/response_data.json", "w") as f:
+    json.dump(results, f)
+
+#converting json to xlsx
 export_json()
+
